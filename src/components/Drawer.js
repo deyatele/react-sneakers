667ac17a9778fbc5f7c5e0fbe035e3e6e385179a
Deyatele <deyatele@gmail.com>
2021-07-31 11:44:49 +0300

    Выход к реакту

---
+++ src/components/Drawer.js
@@ -0,0 +1,65 @@
+export default function Drawer() {
+    return (
+        <div className="overlay" style={{ display: 'none' }}>
+        <div className="drawer">
+          <h2 className="d-flex justify-between mb-30 ">
+            Корзина{' '}
+            <img
+              className="removeBtn cu-p"
+              src="/img/btn-remove-hover.svg"
+              alt="Remove"
+            />
+          </h2>
+          <div className="items">
+            <div className="cartItem d-flex aling-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/item/1.jpg)' }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кросовки</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove-hover.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem d-flex aling-center mb-20">
+              <div
+                style={{ backgroundImage: 'url(/img/item/1.jpg)' }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские кросовки</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove-hover.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="arrow" />{' '}
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+}
