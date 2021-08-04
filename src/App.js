cbe2df55bc93efc3313dcb4dece27bc20f3a764b
Deyatele <deyatele@gmail.com>
2021-08-04 20:40:41 +0300

    ''

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -7,33 +8,69 @@ function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
   React.useEffect(() => {
-    fetch('https://61082c6bd73c6400170d3875.mockapi.io/items')
-      .then((res) => res.json())
-      .then((json) => setItems(json))
+    axios
+      .get('https://61082c6bd73c6400170d3875.mockapi.io/items')
+      .then((res) => setItems(res.data))
       .catch((e) => console.log(e));
+    axios
+      .get('https://61082c6bd73c6400170d3875.mockapi.io/basket')
+      .then((res) => setCartItems(res.data))
+      .catch((e) => console.log(e)); 
   }, []);
   const addToCard = (card) => {
-    setCartItems(prev => [...prev, card])
-  }
+    axios.post('https://61082c6bd73c6400170d3875.mockapi.io/basket', card);
+    setCartItems((prev) => [...prev, card]);
+  };
+  const removeItem = (id) => {
+    axios.delete(`https://61082c6bd73c6400170d3875.mockapi.io/basket/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+  const onSeachInput = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="wrapper clear">
       {cartOpened && (
-        <Drawer onClose={() => setCartOpened(false)} cartItems={cartItems} />
+        <Drawer
+          onClose={() => setCartOpened(false)}
+          cartItems={cartItems}
+          onRemove={(id) => removeItem(id)}
+        />
       )}
       <Header onClickCard={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue ? `Поиск по запросу: ${searchValue}` : 'Все кроссовки'}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск" />
+            {searchValue && (
+              <img
+                className="clear cu-p"
+                onClick={() => setSearchValue('')}
+                src="/img/btn-remove-hover.svg"
+                alt="Clear"
+              />
+            )}
+            <input
+              onChange={onSeachInput}
+              value={searchValue}
+              placeholder="Поиск"
+            />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((arg) => (
-            <Card key={arg.id} {...arg} onPlus={() => addToCard(arg)} />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase()),
+            )
+            .map((arg) => (
+              <Card key={arg.id} {...arg} onPlus={() => addToCard(arg)} />
+            ))}
         </div>
       </div>
     </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,4 +1,4 @@
-export default function Drawer({ onClose, cartItems = [] }) {
+export default function Drawer({ onClose, onRemove, cartItems = [] }) {
   //console.log(cartItems);
   return (
     <div className="overlay">
@@ -12,43 +12,69 @@ export default function Drawer({ onClose, cartItems = [] }) {
             alt="Remove"
           />
         </h2>
-        <div className="items">
-          {/* {console.log(cartItems)} */}
-          {cartItems.map((item) => (
-            <div className="cartItem d-flex aling-center mb-20" key={item.id}>
-              <div
-                style={{ backgroundImage: `url(${item.imageUrl})` }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">{item.title}</p>
-                <b>{item.price} руб.</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn-remove-hover.svg"
-                alt="Close"
-              />
+        {cartItems.length > 0 ? (
+          <div>
+            <div className="items">
+              {/* {console.log(cartItems)} */}
+              {cartItems.map((item) => (
+                <div
+                  className="cartItem d-flex aling-center mb-20"
+                  key={item.id}
+                >
+                  <div
+                    style={{ backgroundImage: `url(${item.imageUrl})` }}
+                    className="cartItemImg"
+                  ></div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{item.title}</p>
+                    <b>{item.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(item.id)}
+                    className="removeBtn"
+                    src="/img/btn-remove-hover.svg"
+                    alt="Close"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="cartTotalBlock">
-          <ul>
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-          <button className="greenButton">
-            Оформить заказ <img src="/img/arrow.svg" alt="arrow" />{' '}
-          </button>
-        </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб.</b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>1074 руб.</b>
+                </li>
+              </ul>
+              <button className="greenButton">
+                Оформить заказ <img src="/img/arrow.svg" alt="arrow" />{' '}
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="cartEmpty aling-center">
+            <img
+              className="mb-20"
+              width="120px"
+              height="120px"
+              src="/img/empty-cart.jpg"
+              alt="BasketNo"
+            />
+            <h2>Корзина пустая</h2>
+            <p className="opacity-6">
+              Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
+            </p>
+            <button onClick={onClose} className="greenButton">
+              <img src="/img/arrow.svg" alt="Arrow" />
+              Вернутся назад
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
